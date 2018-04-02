0248339a36cb78e0e03d94dd926d6e423e36675f
RajPuttu <35489504+RajPuttu@users.noreply.github.com>
2018-04-01 23:44:14 -0700

    Create ἇ☨☩☪☫デト.cpp

---
+++ ἇ☨☩☪☫デト.cpp
@@ -0,0 +1,7 @@
+.Clone from github -- add AR stream as second remote and perform push/pull operations
+
+Yes	
+Isaac: clone from git, and push to AccuRev; also test the sub-directory (e.g. accurev://host:port/stream/dir1/dir2) and make sure contents outside of the directory will not get pushed to AccuRev
+
+2.Clone from AR -- add git as second remote and perform push/pull operations	Yes	
+Isaac: same as above
